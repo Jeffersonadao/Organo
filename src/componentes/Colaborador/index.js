8bf50958ce35f72c5a79f5525dd8a63c74054e5d
jefferson Ferreira <jefferson.a.hp@gmail.com>
2023-08-04 15:02:20 -0300

    Primeiro Commit

---
+++ src/componentes/Colaborador/index.js
@@ -0,0 +1,17 @@
+import './Colaborador.css'
+
+const Colaborador = () => {
+    return (
+        <div className='colaborador'>
+            <div className='cabecalho'>
+                <img src='https://github.com/Jeffersonadao.png' alt='foto de Jefferson Ferreira' />
+            </div>
+            <div className='rodape'>
+                <h4>Jefferson Ferreira</h4>
+                <h5>Developer</h5>
+            </div>
+        </div>
+    )
+}
+
+export default Colaborador
---
+++ src/componentes/Time/index.js
@@ -0,0 +1,19 @@
+import Colaborador from '../Colaborador'
+import './Time.css'
+
+const Time = (props) => {
+
+    const css = { backgroundColor: props.corSecundaria }
+    return(
+
+        
+        <section className='time' style={css} >
+            <h3 style={{ borderColor: props.corPrimaria }} >{props.nome}</h3>
+            <Colaborador />
+            <Colaborador />
+        </section>
+    )
+}
+
+
+export default Time
